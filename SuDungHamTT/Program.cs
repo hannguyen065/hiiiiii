eb6f9a3cf51456c436eb42e8d26530b7679fcc04
trung889 <107554657+hannguyen065@users.noreply.github.com>
2022-06-22 21:26:16 +0700

    hiiii

---
+++ SuDungHamTT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SuDungHamTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("======Menu========");
+            Console.WriteLine("======Menu========");
+            Console.WriteLine("======Menu========");
+            Console.WriteLine("======Menu========");
+        }
+    }
+}
